01c749a5ed201c859ef8868c8fe748f222eaaf32
Ilyass Chahbia <ilchahbi@e1r8p8.1337.ma>
2024-08-19 16:10:35 +0100

    gnl files

---
+++ get_next_line.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ilchahbi <ilchahbi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/01 14:57:42 by ilchahbi          #+#    #+#             */
+/*   Updated: 2024/04/02 13:26:02 by ilchahbi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
+# endif
+
+char	*get_next_line(int fd);
+size_t	ft_strlen(const char *str);
+int		ft_strchr(const char *s, char c);
+char	*ft_strdup(const char *s1);
+char	*ft_strjoin(char *s1, char const *s2);
+
+#endif
